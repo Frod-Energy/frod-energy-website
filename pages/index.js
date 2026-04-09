17da762a41eab2b777dfa2847f764ce65bc96bbe
Frod-Energy <ingar@frod-energy.no>
2026-04-09 20:29:43 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-export default function Home(){return (<h1>FROD Energy</h1>)}
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial", color: "#0b3d2e" }}>
+      <h1>FROD Energy</h1>
+      <p>Nettsiden er under utvikling.</p>
+    </div>
+  );
+}
